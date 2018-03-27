89c45b1ee30884abd7ef5ccf0eef15af295e533c
Tai_Nien_Ko <31752048+tainenko@users.noreply.github.com>
2018-03-27 19:27:06 +0800

    easy

---
+++ 232_Implement Queue using Stacks.py
@@ -0,0 +1,57 @@
+'''
+Implement the following operations of a queue using stacks.
+
+push(x) -- Push element x to the back of queue.
+pop() -- Removes the element from in front of queue.
+peek() -- Get the front element.
+empty() -- Return whether the queue is empty.
+Notes:
+You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
+You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+'''
+
+
+class MyQueue:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.inStack, self.outStack = [], []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: nothing
+        """
+        self.inStack.append(x)
+
+    def pop(self):
+        """
+        :rtype: nothing
+        """
+        self.peek()
+        return self.outStack.pop()
+
+    def peek(self):
+        """
+        :rtype: int
+        """
+        if not self.outStack:
+            while self.inStack:
+                self.outStack.append(self.inStack.pop())
+        return self.outStack[-1]
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return (not self.inStack) and (not self.outStack)
+
+    # Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
---
+++ 28. Implement strStr().py
@@ -0,0 +1,42 @@
+'''
+Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "hello", needle = "ll"
+Output: 2
+Example 2:
+
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+
+'''
+
+
+class Solution:
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        return haystack.index(needle) if needle in haystack else -1
+
+
+class Solution2:
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if not haystack and not needle:
+            return 0
+
+        for i in range(len(haystack)):
+            if haystack[i:i + len(needle)] == needle:
+                return i
+
+        return -1
